be53a08e83474de5bce8da2e7330f188f436add2
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2021-12-12 02:24:21 -0300

    APL-003 - Agregados Entity, Controller, y Repository para Usuario

---
+++ src/main/java/com/informatorio/proyectofinal/controller/UsuarioController.java
@@ -0,0 +1,43 @@
+package com.informatorio.proyectofinal.controller;
+
+import javax.validation.Valid;
+import com.informatorio.proyectofinal.entity.Usuario;
+import com.informatorio.proyectofinal.repository.UsuarioRepository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping(path = "/usuarios")
+public class UsuarioController {
+
+    private final UsuarioRepository usuarioRepository;
+    
+    @Autowired
+    public UsuarioController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @GetMapping
+    public @ResponseBody Iterable<Usuario> obtenerUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    //@GetMapping
+    //public @ResponseBody Iterable<Usuario> obtenerUsuariosDespuesDeFechaDada(
+    //        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaDesde) {
+    //            List<Emprendimientos emprendimientos 
+    //   return usuarioRepository.findAll();
+    //}
+
+    //@PostMapping
+    //public Usuario crearUsuario(@RequestBody Usuario usuario) {
+    //    return usuarioRepository.save(usuario);
+    //}
+
+    @PostMapping
+    public ResponseEntity<?> crearUsuario(@Valid @RequestBody Usuario usuario)  {
+        return new ResponseEntity<>(usuarioRepository.save(usuario), HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/informatorio/proyectofinal/entity/Usuario.java
@@ -0,0 +1,122 @@
+package com.informatorio.proyectofinal.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.persistence.Column;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Email;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotEmpty(message = "El nombre no pude estar vacio.")
+    private String nombre;
+
+    @NotEmpty(message = "El apellido no pude estar vacio.")
+    private String apellido;
+
+    @Email(regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime fechaDeCreacion;
+
+    private String ciudad;
+
+    private String provincia;
+
+    private String pais;
+
+    @Enumerated(EnumType.STRING) 
+    private TipoUsuario tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void getEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public LocalDateTime getFechaDeCreacion() {
+        return fechaDeCreacion;
+    }
+
+    public void setFechaDeCreacion(LocalDateTime fechaDeCreacion) {
+        this.fechaDeCreacion = fechaDeCreacion;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public TipoUsuario getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoUsuario tipo) {
+        this.tipo = tipo;
+    }
+}
