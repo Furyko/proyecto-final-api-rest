b6fe12982055ab77051d2bb35d69fdd7e4e84cc2
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2021-12-13 15:30:12 -0300

    APL-008 - Agregado query para buscar usuarios a partir de una fecha de creación dada

--- src/main/java/com/informatorio/proyectofinal/controller/UsuarioController.java
+++ src/main/java/com/informatorio/proyectofinal/controller/UsuarioController.java
@@ -1,10 +1,14 @@
 package com.informatorio.proyectofinal.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import javax.validation.Valid;
 import com.informatorio.proyectofinal.entity.Usuario;
 import com.informatorio.proyectofinal.repository.UsuarioRepository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -20,9 +24,22 @@ public class UsuarioController {
     }
 
     @GetMapping
-    public @ResponseBody Iterable<Usuario> obtenerUsuarios() {
-        return usuarioRepository.findAll();
+    public ResponseEntity<?> obtenerUsuarios(
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fechaDesde) {
+        if (fechaDesde != null) {
+            List<Usuario> usuarios = usuarioRepository.findByfechaDeCreacionAfter(fechaDesde.atStartOfDay());
+            return new ResponseEntity<>(usuarios, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(usuarioRepository.findAll(), HttpStatus.OK);
     }
+
+    @PostMapping
+    public ResponseEntity<?> crearUsuario(@Valid @RequestBody Usuario usuario) {
+        return new ResponseEntity<>(usuarioRepository.save(usuario), HttpStatus.CREATED);
+    }
+
+    //@DeleteMapping("{id}")
+    //public ResponseEntity<?> eliminarUsuario(@PathVariable Long id) {}
 
     //@GetMapping
     //public @ResponseBody Iterable<Usuario> obtenerUsuariosDespuesDeFechaDada(
@@ -35,9 +52,4 @@ public class UsuarioController {
     //public Usuario crearUsuario(@RequestBody Usuario usuario) {
     //    return usuarioRepository.save(usuario);
     //}
-
-    @PostMapping
-    public ResponseEntity<?> crearUsuario(@Valid @RequestBody Usuario usuario) {
-        return new ResponseEntity<>(usuarioRepository.save(usuario), HttpStatus.CREATED);
-    }
 }
--- src/main/java/com/informatorio/proyectofinal/repository/UsuarioRepository.java
+++ src/main/java/com/informatorio/proyectofinal/repository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package com.informatorio.proyectofinal.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import com.informatorio.proyectofinal.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    List<Usuario> findByfechaDeCreacionAfter(LocalDateTime fecha);
 }
